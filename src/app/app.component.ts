db2f61b17e30d6fc6bb7c54d4818470f7b1cfd25
1Mpedence <85394883+1Mpedence@users.noreply.github.com>
2024-08-15 21:25:24 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-
   @ViewChild('search', { static: false }) search: any;
-  
+
   title = 'countries-rest-api';
 
   countries: any;
@@ -28,88 +31,102 @@ export class AppComponent implements OnInit {
 
   countries2Show: any;
 
-  theme = 'light';
+  theme = Theme.LIGHT;
 
   countrySelected = false;
 
   country: any;
 
   loader = false;
-  
-  searchDebouncer: number;
 
-  constructor(
-    private http: HttpClient
-  ) {}
+  searchDebouncer: ReturnType<typeof setTimeout> | null = null;
+
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.getCountries();
-    this.theme = localStorage.getItem('theme') ?? 'light';
+    this.theme = (localStorage.getItem('theme') as Theme) ?? this.theme;
   }
 
   getCountries() {
     this.loader = true;
-    this.http.get(this.url).pipe(
-      map((data) => {
-        this.countries = data;
-      }), catchError(this.handleError)
-    ).subscribe((result) => {
-      this.countriesCopy = [...this.countries];
-      this.countries2Show = [...this.countries];
-      this.regionList = new Set(this.countries.map((country: { region: string; }) => country?.region));
-      this.loader = false;
+    this.http.get(this.url).subscribe({
+      next: (result) => {
+        this.countries = result;
+        this.countriesCopy = [...this.countries];
+        this.countries2Show = [...this.countries];
+        this.regionList = new Set(
+          this.countries.map((country: { region: string }) => country?.region)
+        );
+        this.loader = false;
+      },
+      error: () => {
+        this.snackBar.open('Error getting Countries', '', {
+          panelClass: ['main-container-tost', 'p-0', 'text-danger'],
+        });
+        this.loader = false;
+      },
     });
   }
 
-  private handleError(error: HttpErrorResponse) {
-    return throwError(error);
-  }
-
-  onRegionChange(): void{
+  onRegionChange(): void {
     this.searchKey = '';
-    this.countries2Show = (this.selected?.length === 0) ?
-      [...this.countries] :
-      this.countries.filter((country: { region: string; }) => this.selected.includes(country?.region));
+    this.countries2Show =
+      this.selected?.length === 0
+        ? [...this.countries]
+        : this.countries.filter((country: { region: string }) =>
+            this.selected.includes(country?.region)
+          );
     this.countriesCopy = this.countries2Show;
   }
 
-  onSearchWrapper(){
-    if(this.searchDebouncer)  clearTimeout(this.searchDebouncer);
+  onSearchWrapper() {
+    if (this.searchDebouncer) clearTimeout(this.searchDebouncer);
     this.searchDebouncer = setTimeout(() => this.onSearch(), 555);
   }
 
-  onSearch(): void{
-    if(!this.searchKey || this.searchKey?.length === 0){
+  onSearch(): void {
+    if (!this.searchKey || this.searchKey?.length === 0) {
       this.onRegionChange();
       return;
     }
-    this.countries2Show = this.countriesCopy.filter((country: { region: string; capital: string; name: { common: string; }; }) => {
-      return (
-        country?.region?.toLowerCase()?.includes(this.searchKey.toLowerCase()) ||
-        country?.capital?.[0]?.toLowerCase()?.includes(this.searchKey.toLowerCase()) || 
-        country?.name?.common?.toLowerCase()?.includes(this.searchKey.toLowerCase())
-      )
-    })
+    this.countries2Show = this.countriesCopy.filter(
+      (country: {
+        region: string;
+        capital: string;
+        name: { common: string };
+      }) => {
+        return (
+          country?.region
+            ?.toLowerCase()
+            ?.includes(this.searchKey.toLowerCase()) ||
+          country?.capital?.[0]
+            ?.toLowerCase()
+            ?.includes(this.searchKey.toLowerCase()) ||
+          country?.name?.common
+            ?.toLowerCase()
+            ?.includes(this.searchKey.toLowerCase())
+        );
+      }
+    );
   }
 
-  themeChange(){
-    this.theme = (this.theme === 'light') ? 'dark' : 'light'; 
+  themeChange() {
+    this.theme = this.theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
     localStorage.removeItem('theme');
     localStorage.setItem('theme', this.theme);
   }
 
-  onCountry(country: any){
+  onCountry(country: any) {
     this.countrySelected = true;
     this.country = country;
-    console.log(country);
   }
 
-  back($event: boolean){
-    if($event)  this.countrySelected = false;
+  back($event: boolean) {
+    if ($event) this.countrySelected = false;
   }
 
-  home(){
+  home() {
     this.back(true);
   }
-
 }
